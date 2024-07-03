4640947620ee86bdd3f8a8fbe865f1f739ee61d9
iamyare <yarephotographer@gmail.com>
2024-07-02 20:48:17 -0600

    Update DivCard component styles to include rounded corners

--- src/components/tecnologies.tsx
+++ src/components/tecnologies.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react'; // Se elimina 'React' ya que no se utiliza directamente
 
 export default function Tecnologies() {
   const tecnologias = ['React', 'NextJS', 'TailwindCSS', 'Framer Motion'];
   const [items, setItems] = useState([...tecnologias, ...tecnologias]); // Duplica inicialmente los elementos
-  const scrollRef = useRef(null);
+  const scrollRef = useRef<HTMLDivElement>(null); // Se especifica el tipo de elemento para useRef
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -13,7 +13,7 @@ export default function Tecnologies() {
         if (isAtEnd) {
           // Reinicia el desplazamiento y duplica los elementos para el efecto infinito
           scrollRef.current.scroll({ left: 0, behavior: 'smooth' });
-          setItems([...items, ...tecnologias]);
+          setItems(prevItems => [...prevItems, ...tecnologias]); // Se usa una función para actualizar el estado basado en el estado anterior
         } else {
           // Desplaza suavemente
           scrollRef.current.scrollBy({ left: 1, behavior: 'smooth' });
@@ -22,13 +22,12 @@ export default function Tecnologies() {
     }, 20); // Ajusta este valor para controlar la velocidad del desplazamiento
 
     return () => clearInterval(interval);
-  }, [items]);
+  }, [tecnologias]); // Se actualiza la dependencia a 'tecnologias' ya que 'items' se actualiza dentro del efecto
 
   return (
     <section className="flex flex-col space-y-5 py-10 container">
       <p className='text-center text-muted-foreground'>Tecnologías empleadas</p>
       <div className="overflow-x-hidden relative" ref={scrollRef}>
-      <div className=' absolute top-0 left-0 w-[10%] h-full bg-gradient-to-r from-background to-transparent'></div>
         <ul className='flex space-x-10 animate-scroll'>
           {items.map((item, index) => (
             <li key={index} className='text-center'>
@@ -36,6 +35,9 @@ export default function Tecnologies() {
             </li>
           ))}
         </ul>
+      <div className=' absolute top-0 left-0 w-[15%] h-full bg-gradient-to-r from-background to-transparent'></div>
+      <div className=' absolute top-0 right-0 w-[15%] h-full bg-gradient-to-l from-background to-transparent'></div>
+
       </div>
     </section>
   );
