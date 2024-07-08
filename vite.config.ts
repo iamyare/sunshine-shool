d8095e9a3d60529224ef3952abea2330ce5c3793
iamyare <yarephotographer@gmail.com>
2024-07-08 09:14:29 -0600

    feat: Agregar base path para la aplicación en vite.config.ts y configurar scripts de predeploy y deploy en package.json

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 
 export default defineConfig({
+  base: '/sunshine-shool',
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
