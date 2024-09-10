f2fe7ec11129fcec05dbe3f4266e83280f2e925c
Yamir Alejandro Rodas Elvir <79547618+iamyare@users.noreply.github.com>
2024-09-10 17:25:20 -0600

    feat: Agregar lazy loading para imágenes en el componente STEAM y FAQ
    Se agregó la funcionalidad de lazy loading para las imágenes en los componentes STEAM y FAQ. Esto mejora el rendimiento de la aplicación al cargar las imágenes de forma diferida, lo que reduce el tiempo de carga inicial de la página.

--- src/components/steam.tsx
+++ src/components/steam.tsx
@@ -6,18 +6,85 @@ import imgHola from '../assets/hola.png'
 import { useState, useEffect } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 
+const svgVariants = {
+  hidden: { opacity: 1 },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      when: 'beforeChildren',
+      staggerChildren: 0.3
+    }
+  }
+}
+
+const pathVariants = {
+  hidden: {
+    pathLength: 0,
+    opacity: 1
+  },
+  visible: {
+    pathLength: 1,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      ease: 'easeInOut'
+    }
+  }
+}
+
+const SVG_PATH = [
+  {
+    path: [
+      {
+        d: 'M8 29C67.3333 18.0209 243.4 -0.64344 473 12.5314',
+        strokeWidth: 16,
+        strokeLinecap: 'round'
+      },
+      {
+        d: 'M136.604 61.165C178.773 48.7423 304.97 21.7612 472.402 13.2188',
+        strokeWidth: 16,
+        strokeLinecap: 'round'
+      },
+      {
+        d: 'M138 60.9386C195.044 54.9318 363.28 50.319 579.871 79.9218',
+        strokeWidth: 16,
+        strokeLinecap: 'round'
+      }
+    ]
+  },
+  {
+    path: [
+      {
+        d: 'M8 7.99986C82.1667 13.8332 260.2 21.9998 379 7.99982',
+        strokeWidth: 16,
+        strokeLinecap: 'round'
+      },
+      {
+        d: 'M261 15.4995C199.167 28.6662 67.5001 56.4013 25.5001 58.0013C-26.9999 60.0013 190.311 58.0015 292 58.0013C393 58.0011 263.5 72.4995 153.5 83.9995',
+        strokeWidth: '16',
+        strokeLinecap: 'round'
+      }
+    ]
+  }
+]
+
 const STEAM_DATA = [
   {
     title: 'Programa',
     span: 'STEAM',
     content:
-      'En Sunshine School, nuestro programa STEAM (Ciencia, Tecnología, Ingeniería, Arte y Matemáticas) está diseñado para preparar a nuestros estudiantes para enfrentar los desafíos del siglo XXI. A través de este enfoque innovador, promovemos el desarrollo de habilidades críticas como el pensamiento analítico, la creatividad, la comunicación efectiva y la colaboración, capacitando a nuestros alumnos para sobresalir en una sociedad globalizada.'
+      'En Sunshine School, nuestro programa STEAM (Ciencia, Tecnología, Ingeniería, Arte y Matemáticas) está diseñado para preparar a nuestros estudiantes para enfrentar los desafíos del siglo XXI. A través de este enfoque innovador, promovemos el desarrollo de habilidades críticas como el pensamiento analítico, la creatividad, la comunicación efectiva y la colaboración, capacitando a nuestros alumnos para sobresalir en una sociedad globalizada.',
+    img: imgHola,
+    path: SVG_PATH[0]
   },
   {
     title: 'Trabajo Educativo Social',
     span: 'TES',
     content:
-      'Nuestros estudiantes de último año se convierten en líderes educativos al impartir talleres de STEAM a niños en riesgo social. Esta iniciativa no solo refuerza su conocimiento, sino que también les permite contribuir activamente al bienestar de su comunidad, promoviendo la igualdad y la inclusión a través de la educación.'
+      'Nuestros estudiantes de último año se convierten en líderes educativos al impartir talleres de STEAM a niños en riesgo social. Esta iniciativa no solo refuerza su conocimiento, sino que también les permite contribuir activamente al bienestar de su comunidad, promoviendo la igualdad y la inclusión a través de la educación.',
+    img: 'https://i.pinimg.com/474x/cb/63/8f/cb638f1a43e04783bbbaec251a6f416f.jpg',
+    path: SVG_PATH[1]
   }
 ]
 
@@ -51,11 +118,11 @@ export default function STEAM() {
 
       <div className=' relative'>
         <aside
-          className='flex w-full min-h-[60vh] rounded-[70px] bg-zinc-800 relative overflow-hidden '
+          className='flex w-full md:h-[60vh] md:max-h-[500px] rounded-[70px] bg-zinc-800  relative overflow-hidden '
           onMouseEnter={() => setIsPaused(true)}
           onMouseLeave={() => setIsPaused(false)}
         >
-          <div className='w-[30%]' />
+          <div className=' w-0 md:w-[30%]' />
           <div className=' flex flex-col w-full gap-2 p-10 md:p-20 lg:px-36 z-10 '>
             <h2 className=' text-muted-foreground text-xl font-medium'>
               STEAM
@@ -70,8 +137,32 @@ export default function STEAM() {
               >
                 <h3 className=' text-5xl font-semibold text-zinc-100'>
                   {active.title}{' '}
-                  <span className=' underline decoration-wavy decoration-secondary'>
+                  <span className=' relative size-fit '>
                     {active.span}
+
+                    <motion.svg
+                      className='absolute -bottom-[80%] left-1/2 -translate-x-1/2  overflow-visible stroke-secondary -z-10'
+                      fill='none'
+                      xmlns='http://www.w3.org/2000/svg'
+                      viewBox='0 0 572 278'
+                      width='150%'
+                      height='100%'
+                      variants={svgVariants}
+                      initial='hidden'
+                      animate='visible'
+                    >
+
+                      {active.path.path.map((path, index) => (
+                        <motion.path
+                          key={index}
+                          d={path.d}
+                          strokeWidth={path.strokeWidth}
+                          strokeLinecap={'round'}
+                          variants={pathVariants}
+                        />
+                      ))}
+
+                    </motion.svg>
                   </span>
                 </h3>
                 <p className=' text-muted-foreground mt-2'>{active.content}</p>
@@ -91,19 +182,30 @@ export default function STEAM() {
           </svg>
         </aside>
 
-        <img src={imgHola} alt='' className=' absolute top-0 -left-10 max-w-[30%] md:max-w-[40%] lg:max-w-full' />
+        <AnimatePresence mode='wait'>
+          <motion.img
+            key={active.title}
+            initial={{ opacity: 0, x: 50 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: -50 }}
+            transition={{ duration: 0.3, ease: 'easeInOut' }}
+            src={active.img}
+            alt={`${active.title} ${active.span} imagen`}
+            className=' absolute top-0 -left-10 max-w-[30%] md:max-w-[40%] lg:max-w-full'
+          />
+        </AnimatePresence>
 
         <Button
           size={'icon'}
           onClick={handlePrev}
-          className=' bg-zinc-200 hover:bg-zinc-400 absolute bottom-10 left-0 rounded-3xl p-6 size-fit -translate-x-1/2 shadow-xl'
+          className=' bg-zinc-200 hover:bg-zinc-400 absolute bottom-10 left-0 rounded-3xl p-6 size-fit -translate-x-1/2 shadow-xl z-10 active:scale-95'
         >
           <ChevronLeft className='size-6 text-zinc-800' />
         </Button>
         <Button
           size={'icon'}
           onClick={handleNext}
-          className=' bg-zinc-200 hover:bg-zinc-400 absolute bottom-10 right-0 rounded-3xl p-6 size-fit translate-x-1/2 shadow-xl'
+          className=' bg-zinc-200 hover:bg-zinc-400 absolute bottom-10 right-0 rounded-3xl p-6 size-fit translate-x-1/2 shadow-xl z-10 active:scale-95'
         >
           <ChevronRight className='size-6 text-zinc-800' />
         </Button>
