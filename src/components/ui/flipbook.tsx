c250c7425d833b49c5ae67007fca02b63c54c8c9
Yamir Alejandro Rodas Elvir <79547618+iamyare@users.noreply.github.com>
2024-10-02 16:29:13 -0600

    feat: Agregar lazy loading para imágenes en los componentes AcademicProgram y navigation
    - Agregar lazy loading para imágenes en los componentes AcademicProgram y navigation.
    - Añadir estilos CSS para el componente Flipbook.

--- src/components/ui/flipbook.tsx
+++ src/components/ui/flipbook.tsx
@@ -1,33 +1,38 @@
-import { cn } from '@/lib/utils';
+import { cn } from '@/lib/utils'
 import React, { useState, forwardRef } from 'react'
 import HTMLFlipBook from 'react-pageflip'
 import { Document, Page, pdfjs } from 'react-pdf'
 
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.mjs`;
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.mjs`
 
 interface PagesProps {
-  children: React.ReactNode;
-  number: number;
+  children: React.ReactNode
+  number: number
 }
 
-const Pages = forwardRef<HTMLDivElement, PagesProps>(({ children, number }, ref) => {
-  return (
-    <div className={cn(
-      //si las paginas no son la ultima ni la primera, enotnces entra una sombra en medio
-      'page',
-      number % 2 === 0 ? 'page-right' : 'page-left'
-      
-    )} ref={ref}>
-      {children}
-      <p>Page number: {number}</p>
-    </div>
-  )
-})
+const Pages = forwardRef<HTMLDivElement, PagesProps>(
+  ({ children, number }, ref) => {
+
+    return (
+      <div
+        className={cn(
+          'page',
+          number === 1 && 'page-cover',
+          number % 2 === 0 ? 'page-right' : 'page-left'
+        )}
+        ref={ref}
+      >
+        {children}
+        <p>Page number: {number}</p>
+      </div>
+    )
+  }
+)
 
 Pages.displayName = 'Pages'
 
 interface FlipbookProps {
-  pdfUrl: string;
+  pdfUrl: string
 }
 
 export default function Flipbook({ pdfUrl }: FlipbookProps) {
@@ -35,7 +40,6 @@ export default function Flipbook({ pdfUrl }: FlipbookProps) {
   const [pdfLoaded, setPdfLoaded] = useState<boolean>(false)
 
   function onDocumentLoadSuccess({ numPages }: { numPages: number }) {
-    console.log('numPages', numPages)
     setNumPages(numPages)
     setPdfLoaded(true)
   }
@@ -43,51 +47,51 @@ export default function Flipbook({ pdfUrl }: FlipbookProps) {
   return (
     <div className='h-screen w-screen flex flex-col gap-5 justify-center items-center bg-gray-900 overflow-hidden'>
       <h1 className='text-3xl text-white text-center font-bold'>FlipBook</h1>
-      
+
       <Document file={pdfUrl} onLoadSuccess={onDocumentLoadSuccess}>
-      {pdfLoaded ? (
-        <HTMLFlipBook
-          width={400}
-          height={570}
-          size='fixed'
-          minWidth={100}
-          maxWidth={500}
-          minHeight={100}
-          maxHeight={500}
-          drawShadow={true}
-          flippingTime={1000}
-          usePortrait={false}
-          startZIndex={0}
-          autoSize={false}
-          maxShadowOpacity={0.5}
-          showCover={true}
-          mobileScrollSupport={false}
-          clickEventForward={false}
-          useMouseEvents={true}
-          swipeDistance={100}
-          showPageCorners={true}
-          disableFlipByClick={false}
-          className="demo-book"
-          style={{}}
-          startPage={0}
-        >
-          {[...Array(numPages)].map((_, index) => (
-            <Pages key={index} number={index + 1}>
-              <Page
-                pageNumber={index + 1}
-                width={400}
-                renderAnnotationLayer={false}
-                renderTextLayer={false}
-              />
-              <p>
-                Page {index + 1} of {numPages}
-              </p>
-            </Pages>
-          ))}
-        </HTMLFlipBook>
-      ) : (
-        <div className='text-white'>Loading PDF...</div>
-      )}
+        {pdfLoaded ? (
+          <HTMLFlipBook
+            width={400}
+            height={570}
+            size='fixed'
+            minWidth={100}
+            maxWidth={500}
+            minHeight={100}
+            maxHeight={500}
+            drawShadow={true}
+            flippingTime={1000}
+            usePortrait={false}
+            startZIndex={0}
+            autoSize={false}
+            maxShadowOpacity={0.5}
+            showCover={true}
+            mobileScrollSupport={false}
+            clickEventForward={false}
+            useMouseEvents={true}
+            swipeDistance={100}
+            showPageCorners={true}
+            disableFlipByClick={false}
+            className='demo-book'
+            style={{}}
+            startPage={0}
+          >
+            {[...Array(numPages)].map((_, index) => (
+              <Pages key={index} number={index + 1}>
+                <Page
+                  pageNumber={index + 1}
+                  width={400}
+                  renderAnnotationLayer={false}
+                  renderTextLayer={false}
+                />
+                <p>
+                  Page {index + 1} of {numPages}
+                </p>
+              </Pages>
+            ))}
+          </HTMLFlipBook>
+        ) : (
+          <div className='text-white'>Loading PDF...</div>
+        )}
       </Document>
     </div>
   )
